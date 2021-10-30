242cb6d658f688375693a65beadd637f7c495141
Scott Huchton <shuchton@gmail.com>
2021-10-30 17:33:44 -0400

    added Showcase component

--- components/Layout.js
+++ components/Layout.js
@@ -2,8 +2,12 @@ import Head from "next/head";
 import styles from "@/styles/Layout.module.css";
 import Header from "./Header";
 import Footer from "./Footer";
+import Showcase from "./Showcase";
+
+import { useRouter } from "next/router";
 
 export default function Layout({ title, keywords, description, children }) {
+  const router = useRouter();
   return (
     <div>
       <Head>
@@ -12,6 +16,7 @@ export default function Layout({ title, keywords, description, children }) {
         <meta name="keywords" content={keywords} />
       </Head>
       <Header />
+      {router.pathname === "/" && <Showcase />}
       <div className={styles.container}>{children}</div>
       <Footer />
     </div>
