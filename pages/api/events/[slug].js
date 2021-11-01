0e89f567709eed5bcc09578e80ba1b0261c58d07
Scott Huchton <shuchton@gmail.com>
2021-11-01 07:18:36 -0400

    added singleevent page

---
+++ pages/api/events/[slug].js
@@ -0,0 +1,11 @@
+const { events } = require("./data.json");
+
+export default function handler(req, res) {
+  if (req.method === "GET") {
+    const evt = events.filter(e => e.slug === req.query.slug) 
+    res.status(200).json(evt);
+  } else {
+    res.setHeader("Allow", ["GET"]);
+    res.status(405).json({ message: `Method ${req.method} is not allowed` });
+  }
+}
