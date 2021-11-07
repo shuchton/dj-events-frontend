be23a8e86f32e29e7eee86af1e188155e5478ec2
Scott Huchton <shuchton@gmail.com>
2021-11-07 10:23:40 -0500

    connected to strapi backend

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -26,12 +26,17 @@ export default function EventPage({ evt }) {
           </a>
         </div>
         <span>
-          {evt.date} at {evt.time}
+          {new Date(evt.date).toLocaleDateString()} at {evt.time}
         </span>
         <h1>{evt.name}</h1>
         {evt.image && (
           <div className={styles.image}>
-            <Image src={evt.image} width={960} height={600} alt="Event image" />
+            <Image
+              src={evt.image.formats.medium.url}
+              width={960}
+              height={600}
+              alt="Event image"
+            />
           </div>
         )}
         <h3>Performers:</h3>
@@ -52,7 +57,7 @@ export default function EventPage({ evt }) {
 }
 
 export async function getStaticPaths() {
-  const res = await fetch(`${API_URL}/api/events`);
+  const res = await fetch(`${API_URL}/events`);
   const events = await res.json();
   const paths = events.map((evt) => ({
     params: { slug: evt.slug },
@@ -65,7 +70,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params: { slug } }) {
-  const res = await fetch(`${API_URL}/api/events/${slug}`);
+  const res = await fetch(`${API_URL}/events?slug=${slug}`);
   const events = await res.json();
 
   return {
